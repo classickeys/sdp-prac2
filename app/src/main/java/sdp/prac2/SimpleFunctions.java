91be09a0ca4cdc16690c35a390607bfcb9adf8a2
Kelechi <kidkmen@gmail.com>
2023-04-18 12:48:12 +0200

    added task 3 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,18 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    
+    public static boolean Task3(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
